85ba7f507610a680a5445b127c94dd5cdd5e5aa8
Donovan Rogers <donovan.rogers2@extendhealth.com>
2016-08-18 19:43:29 -0600

    Added the Interest entity to store the Person's interests and it's associated tests.

---
+++ 1_Domain/PeopleSearch.Domain/Entities/Interest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleSearch.Domain.Entities
+{
+    public class Interest
+    {
+        public string Value { get; protected set; }
+
+        protected Interest()
+        {
+            
+        }
+
+        public static Interest Create(string interest)
+        {
+            if (string.IsNullOrEmpty(interest)) throw new ArgumentException("interest text is required", nameof(interest));
+
+            return new Interest {Value = interest};
+        }
+    }
+}
---
+++ 3_InputOutput/Tests/PeopleSearch.UnitTests/Entities/InterestTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using PeopleSearch.Domain.Entities;
+
+namespace PeopleSearch.UnitTests.Entities
+{
+    [TestFixture]
+    public class InterestTests
+    {
+        [Test]
+        public void Create_ShouldValidateParameters()
+        {
+            // Arrange // Act // Assert
+            Assert.Throws<ArgumentException>(() => Interest.Create(""));
+
+            // Arrange // Act // Assert
+            Assert.Throws<ArgumentException>(() => Interest.Create(null));
+        }
+
+        [Test]
+        public void Create_ShouldSetProperties_WhenPassedValidValues()
+        {
+            // Arrange
+            var interestText = "c#";
+
+            // Act
+            var interest = Interest.Create(interestText);
+
+            // Assert
+            interest.Value.Should().Be(interestText);
+        }
+    }
+}
